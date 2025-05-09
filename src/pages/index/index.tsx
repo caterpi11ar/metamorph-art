432de7012dcf9ebf9e8cb64712af0cc00f11b3a6
DaiQin <daiqin1046@gmail.com>
2025-05-09 20:02:02 +0800

    feat: 🐛 project init

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
-import { Text, View } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import { Button, Text, View } from '@tarojs/components'
+import { chooseImage, useLoad } from '@tarojs/taro'
 import './index.scss'
 
 export default function Index() {
@@ -7,9 +7,25 @@ export default function Index() {
     console.log('Page loaded.')
   })
 
+  const handleChooseImage = async () => {
+    try {
+      const res = await chooseImage({
+        count: 9, // 最多可以选择的图片张数
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      })
+      console.log('选择的图片路径:', res.tempFilePaths)
+      // 这里可以将选择的图片路径传递给图像 AI 工具进行处理
+    }
+    catch (error) {
+      console.error('选择图片失败:', error)
+    }
+  }
+
   return (
     <View className="index">
       <Text>metamorph-art</Text>
+      <Button onClick={handleChooseImage}>选择图片</Button>
     </View>
   )
 }
